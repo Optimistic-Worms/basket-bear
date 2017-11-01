05637f590b11315ca3df4c222c313aa14e7bb842
supertopoz <deepsleepsounds@gmail.com>
2017-11-01 11:04:06 +0900

    added webpack

---
+++ client/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import '../css/styles.css'
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+  render() {
+    return ( <h1>Hello World</h1>)
+  } 
+}
+export default App
