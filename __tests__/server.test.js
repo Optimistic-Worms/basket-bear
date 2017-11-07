0846548039a306b1dcc837082c02f2f90d5715bb
ctnswb <xclai.07@gmail.com>
2017-11-06 18:44:57 -0800

    close server and webpack instance after tests

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -3,26 +3,29 @@ const appServer = require('../server/server');
 
 beforeEach(function() {
   originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-  jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 50000;
 });
 
 afterEach(function() {
   jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
 });
 
-// afterAll(() => {
-//   console.log('closing server');
-//   appServer.server.close();
-// });
+afterAll(() => {
+  console.log('close server');
+  appServer.server.close();
+  console.log('close webpack instance');
+  appServer.webpackDevMiddlewareInstance.close();
+});
+
 
 test('should get index page', () => {
-
-  return request(appServer).get('/').expect(200);
-  // console.log(response.statusCode);
-  //    expect(response.statusCode).toBe(400);
-  //    done();
-  // });
-
-  //expect(4).toBe(4);
+  return request(appServer.app).get('/').expect(200);
 });
+
+test('should test that it fails too', () => {
+  return request(appServer.app).get('/').expect(400);
+});
+
+
+
 
