e573c670521ad69fa3814c75fd42f12bd50e31e8
supertopoz <deepsleepsounds@gmail.com>
2017-11-08 14:13:55 +0900

    random test

--- __tests__/authroutes.test.js
+++ __tests__/authroutes.test.js
@@ -1,24 +1,8 @@
-/*const signup = require('../server/authroutes.js').signup
-const firebasemock = require('firebase-mock');
-const mockdatabase = new firebasemock.MockFirebase();
-const mockauth = new firebasemock.MockFirebase();
-const mocksdk = new firebasemock.MockFirebaseSdk(path => {
-  return path ? mockdatabase.child(path) : mockdatabase;
-}, () => {
-  return mockauth;
-});
-
-
-const firebase = mocksdk.initializeApp(); // can take a path arg to database url
-
-describe('(Container) App', () => {
-  it('should', () => {
-    const actual = mapStateToProps({ui: {}}, {firebase});
-
-    // ** expect firebase here to be mocked **
-    expect(actual.addTodo).not.toBeUndefined();
-    // ...
+test('random test', () => {
+    let a = 1;
+    let b = 1;
+    expect(a).toBe(b);
   })
-})*/
+
 
 
