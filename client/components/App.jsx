16de639ffcb4c95ecdb83b383708a361c658df04
ctnswb <xclai.07@gmail.com>
2017-11-11 17:33:01 -0800

    call ebay search on the client and display first item

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,33 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import '../css/styles.css'
 import axios from 'axios';
+import Search from './Search.jsx'
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
+
     };
 
-    this.searchEbay = this.searchEbay.bind(this);
   }
 
   componentDidMount() {
-    this.searchEbay('Macbook');
-  }
 
-  searchEbay(keyword) {
-    axios.get('/searchEbay', {
-      params: {
-        keyword: keyword
-      }
-    })
-    .then((response) => {
-      console.log(response.data.findItemsByKeywordsResponse[0].searchResult[0].item);
-    })
   }
 
   render() {
-    return ( <h1>Hello World Deploy!</h1>)
+    return (
+      <div>
+        <h1>Hello World Deploy!</h1>
+        <Search/>
+      </div>
+    )
   }
 }
 export default App
