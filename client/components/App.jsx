8cb1f3806adaf2651bbf0b27b29a8d687f641008
supertopoz <deepsleepsounds@gmail.com>
2017-11-16 05:32:08 +0900

    add google signin

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,24 +5,27 @@ import axios from 'axios';
 import Search from './Search.jsx'
 import Navbar from './Navbar.jsx'
 
+import LoginCard from './user/LoginCard.jsx'
+
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-
     };
-
   }
 
   componentDidMount() {
-
   }
-
+  
   render() {
     return (
       <div>
+
       <Navbar/>
       <Search/>
+        <LoginCard/>
+        <h1>Hello World Deploy!</h1>
+        <Search/>
       </div>
     )
   }
