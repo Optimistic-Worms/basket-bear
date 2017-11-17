3ee39dbbed6f3e1f0ab03b1bb63ff78e5f7a467c
Nick Havens <npahavens@gmail.com>
2017-11-17 13:01:29 -0600

    add apiUser on request
    use passport for authentication
    
    correct starter image
    
    testing deployment
    
    find user in db and log it
    
    add passport back
    
    correct starter image
    
    testing deployment
    
    testing deployment

--- server/controllers/apiUser.js
+++ server/controllers/apiUser.js
@@ -1,7 +1,16 @@
 const db = require('../../db/db-config.js');
+//const auth = require('./auth.js')
+const Promise = require('bluebird');
 
 exports.addApiUser = (username, password) => {
-
+  return new Promise((resolve, reject) => {
+    db.collection('apiUsers').add({
+      username: username,
+      password: password
+    })
+    .then(ref => resolve(ref))
+    .catch(err => reject(err));
+  });
 }
 
 exports.loginApiUser = (username, password) => {
@@ -19,3 +28,4 @@ exports.getProductData = (productObj) => {
 exports.getMerchantData = () => {
 
 }
+
