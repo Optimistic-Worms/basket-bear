73907338d5b704feec872a989f39e64bac8e7c83
Nick Havens <npahavens@gmail.com>
2017-11-17 16:45:16 -0600

    use passport for authentication

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 #use a base node image
-FROM nphavens/basket-starter
+FROM nphavens/basket
 
 WORKDIR /app
 
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -4,6 +4,10 @@ const db = require('../../db/db-config.js');
 
 passport.use(new BasicStrategy(
   (username, password, callback) => {
-
+    db.collection('apiUsers').where('username', '==', username).get()
+    .then(snapshot => {
+      console.log(snapshot)
+    });
+    .catch(err => console.log('cannot find user: ', username));
   })
 );
