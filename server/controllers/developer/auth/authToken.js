77a8ee7048a844214a017d1c5e67ba1a1084fa7b
Nick Havens <npahavens@gmail.com>
2017-11-27 14:22:35 -0600

    uae authToken controller to store and verofy tokens

---
+++ server/controllers/developer/auth/authToken.js
@@ -0,0 +1,37 @@
+const Promise = require('bluebird');
+const db = require('../../../../db/db-config.js');
+const encrypt = require('../../../helpers/encryption.js');
+
+exports.addToDb = (clientId, token, email) => {
+  return new Promise((resolve, reject) => {
+    db.collection('apiAuthTokens').add({
+      value: token,
+      user: email,
+      clientId: clientId
+    }).then(tokenRef => {
+      console.log('Token added');
+      resolve(tokenRef);
+    })
+    .catch(err => reject(err));
+  });
+};
+
+exports.findByValue = (tokenValue, callback) => {
+  console.log('getting token data')
+  db.collection('apiAuthTokens').get()
+  .then(tokens => {
+    if (tokens.docs.length) {
+      for (let i = 0; i < tokens.docs.length; i++) {
+        const tokenRef = tokens.docs[i];
+        const tokenData = tokenRef.data();
+        if (encrypt.verifyPasswordSync(tokenValue, tokenData.value)) {
+          return callback(null, tokenRef, tokenData);
+        }
+      }
+      return callback('no matching token found');
+    } else {
+      return callback('no matching token found');
+    }
+  })
+  .catch((err) => callback('no matching token found'));
+};
