87cf984e127774756c6bef916b193274f1e56bc0
ctnswb <xclai.07@gmail.com>
2017-12-02 16:19:27 -0800

    fixed amazon item price listing, diplays the sale price if any, instead of the regular pricing

--- client/components/Product.jsx
+++ client/components/Product.jsx
@@ -25,35 +25,3 @@ const Product = (props) => {
 
 
 export default Product;
-/*
-    <div className="card"> ITEM 1
-      <img className="product-image" src={props.item && props.item.imageUrl} />
-      <h3>{ props.item && props.item.name }</h3>
-      <div className="item-info">
-      <p>{ props.item && props.item.merchant }</p>
-       <p className="price">${ props.item && props.item.price }</p>
-      <p><button className="button button--product" onClick= { ()=> {window.open(props.item.link);}} >Buy Product</button>
-      <button className="button button--product" onClick= {()=>{ props.addItem(props.item) } } >Add to Shopping List</button>
-
-      </p>
-
-      </div>
-
-    </div> */
-
-      // {
-      //   !props.added && <button className="button button--product" onClick= {()=>{ props.addItem(props.item) } } >Add to Shopping List</button>
-      // }
-      // {
-      //   props.added && <button className="button button--product" onClick= {()=>{} } > Remove From Shopping List</button>
-      // }
-// var Product = {
-//   id: Number,
-//   name: String,
-//   imageUrl: String,
-//   description: String,
-//   merchant: String,
-//   link: String,
-//   priceHistory: Array,
-//   userPricing: Array of tuples [user, price]
-// }
