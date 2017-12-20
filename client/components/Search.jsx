71cd0063d4fe7b768452953c81e8a801b3d57227
ArtemisD <dianah456@gmail.com>
2017-12-19 22:45:29 -0500

    update watch card info

--- client/components/Search.jsx
+++ client/components/Search.jsx
@@ -97,7 +97,7 @@ class Search extends React.Component {
       var product = {
         id: searchResults[i].ASIN[0],
         name: searchResults[i].ItemAttributes[0].Title[0],
-        merchant: <i className="fa fa-amazon" aria-hidden="true"></i>,
+        merchant: 'amazon',
         link: searchResults[i].DetailPageURL[0],
       }
 
