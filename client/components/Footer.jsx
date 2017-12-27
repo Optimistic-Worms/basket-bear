b7f4b08c20c8a9c8e3eb34c6f9fe97b50dd9bf36
ArtemisD <dianah456@gmail.com>
2017-12-26 22:10:33 -0500

    add link to github

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Footer = (props) => (
   <footer >
     <div className="footer-about"></div>
-    <div>filler</div>
-    <div>
+    <div className="footer-github"><a href="https://github.com/Optimistic-Worms"><i className="fa fa-github" aria-hidden="true"></i></a></div>
+    <div className="footer-developer">
       <p><i className="fa fa-shopping-basket" aria-hidden="true"></i> Budget Basket Developers</p>
     <div className="footer-link" onClick={props.handleSwitch}>Documentation</div>
   </div>
