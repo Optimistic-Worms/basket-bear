f6a93430a212f67d1aa757b796d126ae0d092ae6
ArtemisD <dianah456@gmail.com>
2017-12-26 21:53:43 -0500

    add developer column in footer

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 const Footer = (props) => (
   <footer >
-    <div className="footer-link" onClick={props.handleSwitch}>Developers</div>
+    <div className="footer-about"></div>
+    <div>filler</div>
+    <div>
+      <p><i className="fa fa-shopping-basket" aria-hidden="true"></i> Budget Basket Developers</p>
+    <div className="footer-link" onClick={props.handleSwitch}>Documentation</div>
+  </div>
   </footer>
 )
 
