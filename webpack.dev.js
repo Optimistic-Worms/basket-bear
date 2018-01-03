2e4204f59d364cd16b8750e5612cde0d96f8ff76
Nick Havens <npahavens@gmail.com>
2018-01-03 12:38:37 -0600

    use hot midlleware only in dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,7 @@ const webpack = require('webpack');
 
 module.exports = merge(common, {
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
      "HOT": process.env.HOT
     })
