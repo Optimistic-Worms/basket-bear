eae0a34ce2ac2d7a9257403e63a94bd7bf414bed
ArtemisD <dianah456@gmail.com>
2018-01-06 18:40:05 -0500

    update terms to align with COPPA requirements

--- client/components/TermsAndConditions.jsx
+++ client/components/TermsAndConditions.jsx
@@ -12,7 +12,7 @@ const TermsAndConditions = (props) => {
 
     <p>These terms and conditions govern your use of this website; by using this website, you accept these terms and conditions in full.   If you disagree with these terms and conditions or any part of these terms and conditions, you must not use this website.
 
-    You must be at least 13 years of age to use this website.  By using this website and by agreeing to these terms and conditions you warrant and represent that you are at least 13 years of age.
+    You must be at least 18 years of age to use this website.  By using this website and by agreeing to these terms and conditions you warrant and represent that you are at least 18 years of age.
 
     This website uses cookies.  By using this website and agreeing to these terms and conditions, you consent to Basket Bear's use of cookies in accordance with the terms of Basket Bear's privacy policy.
     </p>
