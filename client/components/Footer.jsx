e8e68a178a334ee3548e0d4c62e05116fdb927da
ArtemisD <dianah456@gmail.com>
2018-01-06 15:02:54 -0500

    style terms and condition page

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -5,6 +5,7 @@ const Footer = (props) => (
     <div className="footer-about">
     <div><a target="_blank" href="#"><i className="fa fa-envelope-o" aria-hidden="true"></i></a></div>
     <div><a target="_blank" href="https://github.com/Optimistic-Worms"><i className="fa fa-github" aria-hidden="true"></i></a></div>
+        <div><a target="_blank" href="$"><i className="fa fa-info-circle" aria-hidden="true"></i></a></div>
   </div>
   <div className="copyright">
     <p>© 2018 Basket Bear&nbsp;-&nbsp;</p><a href="/terms">Terms of Use</a>
