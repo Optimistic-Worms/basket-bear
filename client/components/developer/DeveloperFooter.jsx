589f1b9ad6f6c6d24d26f918c77791c2364859f9
ArtemisD <dianah456@gmail.com>
2018-01-06 19:31:35 -0500

    align api footer with client footer

--- client/components/developer/DeveloperFooter.jsx
+++ client/components/developer/DeveloperFooter.jsx
@@ -3,9 +3,23 @@ import { Link } from 'react-router-dom';
 
 const DeveloperFooter = (props) => (
   <footer >
-    <Link to="/" className="footer-link">
-    Back to Basket Bear
-    </Link>>
+    <div className="footer-about">
+    <div><a target="_blank" href="#"><i className="fa fa-envelope-o" aria-hidden="true"></i></a></div>
+    <div><a target="_blank" href="https://github.com/Optimistic-Worms"><i className="fa fa-github" aria-hidden="true"></i></a></div>
+        <div><a target="_blank" href="$"><i className="fa fa-info-circle" aria-hidden="true"></i></a></div>
+  </div>
+  <div className="copyright">
+    <p>© 2018 Basket Bear&nbsp;-&nbsp;</p>
+    <Link to="/terms">
+    Terms of Use
+    </Link>
+    </div>
+    <div className="footer-developer">
+      <p><i className="fa fa-shopping-basket" aria-hidden="true"></i> Basket Bear Developers</p>
+      <Link to="/" className="footer-link">
+      Back to Basket Bear
+      </Link>
+    </div>
   </footer>
 )
 
