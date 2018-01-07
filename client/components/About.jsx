8481d5d49ce89945e361ddca66b4c3c493bb633e
ArtemisD <dianah456@gmail.com>
2018-01-07 00:24:14 -0500

    add about us section

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -6,18 +6,26 @@ const About = (props) => (
   <div className="about">
     <div className="about intro-card">
       <h1>OPTIMISTIC WORMS</h1>
-      <h2>Software Engineers</h2>
       <button className="button button-about"><a href="#about">About us</a></button>
     </div>
 
     <div id="about" className="about-profile-section">
-      <div className="about-worms"></div>
+      <div className="about-worms">
+        <h1>WHO WE ARE</h1>
+        <hr />
+        <div className="about-attributes">
+          <i className="fa fa-cogs" aria-hidden="true"></i>
+          <i className="fa fa-desktop" aria-hidden="true"></i>
+          <i className="fa fa-rocket" aria-hidden="true"></i>
+
+        </div>
+      </div>
       <div className="about-profile">
         <img></img>
-        <h3>name</h3>
+        {/* <h3>name</h3> */}
 
         <div className="about-paragraph">
-          <h3>title</h3>
+          {/* <h3>title</h3> */}
         <p></p>
       </div>
 
