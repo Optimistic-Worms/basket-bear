6abad20ca29db72cba06954b0cf6450c6e184a45
ArtemisD <dianah456@gmail.com>
2018-01-06 21:05:37 -0500

    add margins appropriately for signed in users and signed out users

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -2,8 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const About = (props) => (
-  <div>
-    test
+  <div className="about">
+    <div className="about-intro">
+      Optimistic Worms
+    </div>
+
+    <div className="about-profile-section">
+      <div className="about-profile">
+        <img></img>
+        <h3>name</h3>
+
+        <div className="about-paragraph">
+          <h3>title</h3>
+        <p></p>
+      </div>
+      
+      </div>
+    </div>
+
   </div>
 )
 
--- client/components/developer/DeveloperFooter.jsx
+++ client/components/developer/DeveloperFooter.jsx
@@ -6,7 +6,9 @@ const DeveloperFooter = (props) => (
     <div className="footer-about">
     <div><a target="_blank" href="#"><i className="fa fa-envelope-o" aria-hidden="true"></i></a></div>
     <div><a target="_blank" href="https://github.com/Optimistic-Worms"><i className="fa fa-github" aria-hidden="true"></i></a></div>
-        <div><a target="_blank" href="$"><i className="fa fa-info-circle" aria-hidden="true"></i></a></div>
+        <div><Link to="/about">
+        <i className="fa fa-info-circle" aria-hidden="true"></i>
+        </Link></div>
   </div>
   <div className="copyright">
     <p>© 2018 Basket Bear&nbsp;-&nbsp;</p>
