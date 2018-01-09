3bba09644c3c059d37f17a6b69c531903f5663eb
ArtemisD <dianah456@gmail.com>
2018-01-08 23:12:41 -0500

    users can click on image to open link to product

--- client/components/Product.jsx
+++ client/components/Product.jsx
@@ -5,7 +5,7 @@ const Product = (props) => {
 
   return (
     <div className="card">
-      <img className="product-image" src={props.item && props.item.imageUrl} />
+      <a target="_blank" href={props.item.link}><img className="product-image" src={props.item && props.item.imageUrl} /></a>
       <a target="_blank" href={props.item.link}><h4>{ props.item && props.item.name }</h4></a>
       <div className="product-footer">
         <div className="item-info">
