bcd961411dc9efd341819c07321a12003f4ab5cb
ArtemisD <dianah456@gmail.com>
2018-01-10 22:24:15 -0500

    update messaging to developers

--- client/components/developer/JoinApiTout.jsx
+++ client/components/developer/JoinApiTout.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 class JoinApiTout extends React.Component {
 
   render() {
-    return (<div>
+    return (<div className="developer-hometout">
 
       <div className="join-developer">
         <div className="join-developer-column">
@@ -12,6 +12,7 @@ class JoinApiTout extends React.Component {
             <i className="fa fa-shopping-basket" aria-hidden="true"></i>
           </h1>
           <h2>Sign up for a Free Account!</h2>
+          <p>Join Basket Bear Developer and start making API calls today</p>
           <Link to="/dev/login">
             <button className="button button--hometout">Sign Up</button>
           </Link>
@@ -36,10 +37,12 @@ class JoinApiTout extends React.Component {
 
       </div>
 
-      <div className="narrow">
+      <div className="join-developer-support">
         <h1>Discover what you can do with Basket Bear's API</h1>
-        <h4>Create end-to-end selling applications to help sellers with their selling activity on eBay and Amazon. Discover the most likely prices under which customers are most for items to purchase. Our API helps you create applications that enable sellers to grow their business.</h4>
-        <button className="button">read this</button>
+        <p>Create end-to-end applications to help sellers with their selling activity on eBay and Amazon. Discover the ideal price range for products. Our API helps you create applications that enable sellers to grow their business.</p>
+        <Link to="/dev/docs">
+        <button className="button button--hometout">Get Started Now</button>
+      </Link>
       </div>
 
     </div>)
