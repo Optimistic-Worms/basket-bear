2d77cbf0bfa3583deab705852f9df7d8ce7a8f6a
Nick Havens <npahavens@gmail.com>
2018-01-10 21:50:02 -0600

    client secret shows only on succesful signup

--- client/components/developer/apiUser/ClientSecret.jsx
+++ client/components/developer/apiUser/ClientSecret.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 import axios from 'axios'
 import Promise from 'bluebird';
 
-const ClientSecret = ({ secret }) => {
+const ClientSecret = (props) => {
   const getNewSecret = () => {
 
   }
-
+  console.log('SECRET: ', props.secret)
   return (
   <div>{
-  {secret} ?
+  props.secret ?
     <div>
-      <div>*********</div>
-      <div>If you lost your Client Secret you must reset it</div>
+      <div>{props.secret}</div>
+      <div>Store your Client Secret in a safe place. You will not be able to retrieve your client secret after you log out. If you lose your Client Secret you can reset it</div>
     </div>
     :
     <div>
-      <div>secret</div>
-      <div>Store your Client Secret in a safe place. You will not be able to retrieve your client secret after you log out. If you lose your Client Secret you can reset it</div>
+      <div>*********</div>
+      <div>If you lost your Client Secret you must reset it</div>
     </div>
   }
    <div className="button button--remove"
