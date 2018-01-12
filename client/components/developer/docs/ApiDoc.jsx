f9044c8066407e9b65ae65ec31a0014b4d4087f9
Nick Havens <npahavens@gmail.com>
2018-01-11 21:58:06 -0600

    doc nav adjustments

--- client/components/developer/docs/ApiDoc.jsx
+++ client/components/developer/docs/ApiDoc.jsx
@@ -7,31 +7,29 @@ import EndpointReference from './EndpointReference.jsx';
 class ApiDoc extends React.Component {
 
   componentDidMount() {
-    window.scrollTo(0,0);
   }
-
 
   render() {
     return (
       <BrowserRouter>
         <div className="api-doc">
-        <div className="sticky">
-          <div className="api-doc-nav">
-                <h3>Contents</h3>
-            <NavLink to="/dev/docs/start" activeClassName="selected">
-              <h5>Getting Started</h5>
-            </NavLink>
-            <NavLink to="/dev/docs/authorization" activeClassName="selected">
-            <h5>Authorization Guide</h5>
-            </NavLink>
-            <NavLink to="/dev/docs/endpoints" activeClassName="selected">
-            <h5>API Endpoint Reference</h5>
-            </NavLink>
+            <div className="api-doc-nav">
+              <h3>Contents</h3>
+              <NavLink to="/dev/docs/start" activeClassName="selected">
+                <h5>Getting Started</h5>
+              </NavLink>
+              <NavLink to="/dev/docs/authorization" activeClassName="selected">
+              <h5>Authorization Guide</h5>
+              </NavLink>
+              <NavLink to="/dev/docs/endpoints" activeClassName="selected">
+              <h5>API Endpoint Reference</h5>
+              </NavLink>
+            </div>
+          <div className="api-doc-body">
+            <Route exact path="/dev/docs/start" component={GettingStarted}/>
+            <Route path="/dev/docs/authorization" component={AuthorizationGuide}/>
+            <Route path="/dev/docs/endpoints" component={EndpointReference}/>
           </div>
-          </div>
-          <Route exact path="/dev/docs/start" component={GettingStarted}/>
-          <Route path="/dev/docs/authorization" component={AuthorizationGuide}/>
-          <Route path="/dev/docs/endpoints" component={EndpointReference}/>
         </div>
       </BrowserRouter>
     )
