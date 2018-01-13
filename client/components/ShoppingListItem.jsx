65ce409ef5b15aa98570c572dbadc5d8fedf9ed9
ctnswb <xclai.07@gmail.com>
2018-01-13 14:46:49 -0800

    format prices on watch list

--- client/components/ShoppingListItem.jsx
+++ client/components/ShoppingListItem.jsx
@@ -31,13 +31,13 @@ class ShoppingListItem extends React.Component {
             <div className="item-info-merchant">
             <p>{ this.props.item && this.props.item.merchant }</p>
           { this.props.item.available &&
-             <div className="current-price">Current Price <span className="current-price-dollar">${ this.props.item && this.props.item.currentPrice }</span></div>
+             <div className="current-price">Current Price <span className="current-price-dollar">${ this.props.item && this.formatPrice(this.props.item.currentPrice)}</span></div>
             }
           { !this.props.item.available &&
             <p className="current-price">This item is no longer available</p> }
           </div>
           <div className="item-info-merchant">
-            <p className="price">Price when added ${ this.props.item && this.props.item.price }</p>
+            <p className="price">Price when added ${ this.props.item && this.formatPrice(this.props.item.price) }</p>
 
             <p className="current-price">Watch Price <span className="current-price-dollar">$
             {this.props.item && this.formatPrice(this.props.item.watchPrice)}</span></p>
