49e55308207b488484ad78405b5fe21187f2d8e3
ArtemisD <dianah456@gmail.com>
2018-01-13 16:35:12 -0500

    remove mail icon and link

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Footer = (props) => (
   <footer >
     <div className="footer-about">
-    <div><a target="_blank" href="#"><i className="fa fa-envelope-o" aria-hidden="true"></i></a></div>
+    {/* <div><a target="_blank" href="#"><i className="fa fa-envelope-o" aria-hidden="true"></i></a></div> */}
     <div><a target="_blank" href="https://github.com/Optimistic-Worms"><i className="fa fa-github" aria-hidden="true"></i></a></div>
         <div><Link to="/about">
         <i className="fa fa-info-circle" aria-hidden="true"></i>
