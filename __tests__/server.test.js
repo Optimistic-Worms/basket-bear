047b257af75a2f72b7a10a9a043f003d4919ec7f
Nick Havens <npahavens@gmail.com>
2018-02-17 18:34:40 -0600

    serparate server.js and app.js for testing

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,21 +1,21 @@
 const request = require('supertest');
 const appServer = require('../server/server');
 
-beforeEach(function() {
-  originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-  jasmine.DEFAULT_TIMEOUT_INTERVAL = 50000;
-});
+// beforeEach(function() {
+//   originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+//   jasmine.DEFAULT_TIMEOUT_INTERVAL = 50000;
+// });
 
-afterEach(function() {
-  jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
-});
+// afterEach(function() {
+//   jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+// });
 
-afterAll(() => {
-  console.log('close server');
-  appServer.server.close();
-  console.log('close webpack instance');
-  appServer.webpackDevMiddlewareInstance.close();
-});
+// afterAll(() => {
+//   console.log('close server');
+//   appServer.server.close();
+//   console.log('close webpack instance');
+//   appServer.webpackDevMiddlewareInstance.close();
+// });
 
 
 
